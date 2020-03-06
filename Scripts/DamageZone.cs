8c113d80af55650457dce81497c402d1fd1eb95e
Bronan-the-Enjoyer <61307860+Bronan-the-Enjoyer@users.noreply.github.com>
2020-03-06 05:37:35 -0600

    Added health, damage, Collision, Enemy

---
+++ Scripts/DamageZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
